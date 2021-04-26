201314b87b8c39be48c73e9200d5b925ad517410
b-s-x <b.s.x.mars@gmail.com>
2021-04-26 22:13:29 +0300

    create component CardList; added animation

---
+++ client/src/components/Card/CardList.tsx
@@ -0,0 +1,14 @@
+import React, { useEffect } from 'react'
+import gsap from 'gsap'
+import { animCardList } from '../../utils/animation'
+import styles from './card_list.module.scss'
+
+export const CardList = () => {
+  useEffect(() => {
+    animCardList(`.${styles.card__container}`)
+  }, [])
+
+  return (
+    <div className={styles.card__container}></div>
+  )
+}
