f7f007ef747fe181e0846c5d9c2ef45a2c392834
b-s-x <b.s.x.mars@gmail.com>
2021-04-26 15:20:12 +0300

    create component Header

---
+++ client/src/components/Header/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './header.module.scss'
+
+export const Header = () => {
+  return (
+    <div className={styles.header__container}>
+      <div className={styles.header__item}>BSX_MARS</div>
+    </div>
+  )
+}
