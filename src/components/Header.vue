3f995413b082269c81019169d73426c27aa546ea
b-s-x <b.s.x.mars@gmail.com>
2022-01-01 13:19:40 +0300

    @/components/Header: add header component

---
+++ src/components/Header.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import { useAnimationHeader } from '../hooks/animation/index';
+
+onMounted(() => useAnimationHeader(
+  '.header_item',
+  '.header_container',
+  '.header_box',
+))
+
+</script>
+
+<template>
+  <div class="header_container">
+    <div class="header_box">
+      <div class="header_item">
+        BSX_MARS
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+@import "@common";
+
+.header {
+  &_container {
+    background: black;
+    height: 100vh;
+    overflow: hidden;
+    position: relative;
+  }
+
+  &_box {
+    font-family: "Black Ops One";
+    font-size: 200px;
+    // position: absolute;
+    // top: 50%;
+    // width: 100%;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    margin-right: -50%;
+    transform: translate(-50%, -50%)
+  }
+
+  &_item {
+    color: white;
+    padding-left: 50px;
+  }
+}
+
+</style>
